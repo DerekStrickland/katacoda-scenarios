835e504a19d9b771147c2aeb29374eec7f33ae09
DerekStrickland <1111455+DerekStrickland@users.noreply.github.com>
2020-06-09 09:10:31 -0400

    Remvoved minikube specific provisioning scripts.

--- k8s_minikube/provision.courseData.sh
+++ k8s_minikube/provision.courseData.sh
@@ -26,12 +26,7 @@ useradd consul --create-home -G docker -s /bin/bash
 cp /root/dc1-values.yml /home/consul
 cp /root/dc2-values.yml /home/consul
 
-log "Starting first Kubernetes cluster...this might take up to 5 minutes."
-
-
-runuser -l consul -c "minikube start --vm-driver=docker -p dc1 -v 8 --memory 1024"
-
-runuser -l consul -c "minikube start --vm-driver=docker -p dc2 -v 8 --memory 1024"
+log "Starting first Kubernetes cluster..."
 
 
 # minikube start --wait=true
