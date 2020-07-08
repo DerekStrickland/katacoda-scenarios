c1a53c521b67e4e6dd418ef0558e048669c1fc15
DerekStrickland <1111455+DerekStrickland@users.noreply.github.com>
2020-07-08 15:01:34 -0400

    kind create cluster

--- secure-consul-on-kubernetes/provision.courseData.sh
+++ secure-consul-on-kubernetes/provision.courseData.sh
@@ -46,6 +46,8 @@ curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.8.1/kind-$(uname)-amd64
 chmod +x ./kind
 mv ./kind /usr/local/bin/kind
 
+kind create cluster --name dc1
+
 ## ================================
 log "Installing Helm ${HELM_VERSION}"
 
